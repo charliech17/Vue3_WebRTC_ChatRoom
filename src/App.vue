871a6b6d57dd8c7e26a029455f7d44265dab5e8a
Josh <josh_chang@JoshdeMacBook-Pro.local>
2023-02-05 17:36:37 +0800

    add error handle

--- src/App.vue
+++ src/App.vue
@@ -11,16 +11,18 @@
   <div>
     <textarea cols="30" rows="10" @input="handleTextareaInput" v-model="state.textAreaValue"></textarea>
   </div>
+
 </template>
 
 <script setup lang="ts">
 import { Peer } from "peerjs";
 import { nextTick, reactive,ref, onUnmounted} from 'vue'
 
-const state:{isconnect: boolean,inputConnectID: string,textAreaValue: string} = reactive({
+const state:{isconnect: boolean,inputConnectID: string,textAreaValue: string,isError: boolean} = reactive({
   isconnect: false,
   inputConnectID: '',
   textAreaValue: '',
+  isError: false,
 })
 
 // let peer = new Peer();
@@ -100,68 +102,83 @@ function reInitPeer(initId?: string) {
   peer = initId ? new Peer(initId) : new Peer()
 }
 
+let countErrorTime = 0
 function initEventListenr() {
   // 1 接收連接
-peer.on('open',(myId)=> {
-    peerId.value = myId
-})
-
-peer.on("connection", (conn) => {
-    conn.on("data", (data) => {
-      console.log('data!!!',data)
-      state.isconnect = true
-      // @ts-ignore
-      state.textAreaValue = data
-    });
-    conn.on("open", () => {
-      conn.send(state.textAreaValue);
-    });
-});
-
-peer.on("call", (call) => {
-  state.isconnect = true
-  // @ts-ignore
-  const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia; 
-	getUserMedia(
-		{ video: true, audio: true },
-    (stream: MediaStream) => {
-			call.answer(stream); // Answer the call with an A/V stream.
-			call.on("stream", (remoteStream) => {
-        console.log(remoteStream)
-				const video = document.querySelector("#streamVideo") as HTMLVideoElement
-        video.srcObject = remoteStream;
-        video.onloadedmetadata = () => {
-          video.play();
-        };
-			})
-    },
-    (err: Error) => {
-      console.error("Failed to get local stream", err);
-    }
-	)
-})
-
-peer.on('close', function() { 
-  state.isconnect = false
-  const video = document.querySelector('#streamVideo') as HTMLVideoElement
-  stopStreamedVideo(video)
- });
-}
-
-
-window.addEventListener('focus',resetPeer)
-function resetPeer() {
-  nextTick(()=> {
-    console.log('peer.disconnected?',peer.disconnected,peer.id)
-    if(peer.disconnected || !peer.id ) {
-      reInitPeer(peerId.value)
-      initEventListenr()
-    }
+  peer.on('open',(myId)=> {
+      peerId.value = myId
   })
+
+  peer.on("connection", (conn) => {
+      conn.on("data", (data) => {
+        console.log('data!!!',data)
+        state.isconnect = true
+        // @ts-ignore
+        state.textAreaValue = data
+      });
+      conn.on("open", () => {
+        conn.send(state.textAreaValue);
+      });
+  });
+
+  peer.on("call", (call) => {
+    state.isconnect = true
+    // @ts-ignore
+    const getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia; 
+    getUserMedia(
+      { video: true, audio: true },
+      (stream: MediaStream) => {
+        call.answer(stream); // Answer the call with an A/V stream.
+        call.on("stream", (remoteStream) => {
+          console.log(remoteStream)
+          const video = document.querySelector("#streamVideo") as HTMLVideoElement
+          video.srcObject = remoteStream;
+          video.onloadedmetadata = () => {
+            video.play();
+          };
+        })
+      },
+      (err: Error) => {
+        console.error("Failed to get local stream", err);
+      }
+    )
+  })
+
+  peer.on('close', function() { 
+    state.isconnect = false
+    const video = document.querySelector('#streamVideo') as HTMLVideoElement
+    stopStreamedVideo(video)
+ });
+
+ peer.on('error',(error)=> {
+  countErrorTime++
+  reConnectServer()
+  if(countErrorTime > 4) {
+    countErrorTime = 0
+    alert('錯誤發生：'+error)
+  }
+ })
 }
-onUnmounted(()=> {
-  window.removeEventListener('focus',resetPeer)
-})
+
+function reConnectServer() {
+  reInitPeer(peerId.value)
+  initEventListenr()
+}
+
+
+// window.addEventListener('focus',resetPeer)
+// function resetPeer() {
+//   nextTick(()=> {
+//     console.log('peer.disconnected?',peer.disconnected,peer.id)
+//     if(peer.disconnected || !peer.id ) {
+//       reInitPeer(peerId.value)
+//       initEventListenr()
+//     }
+//   })
+// }
+// onUnmounted(()=> {
+//   window.removeEventListener('focus',resetPeer)
+// })
 </script>
 
 <style>
